952327bbb0a8bd51cffca21bd799428e0cbf54f7
Wrpx <moo.wiraphat@gmail.com>
2025-02-03 01:16:36 +0700

    update

--- src/app/core/service/users.service.ts
+++ src/app/core/service/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
-import { BehaviorSubject, Observable, map, of, catchError, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError, map } from 'rxjs';
 import { User } from '../models/user.model';
 
 @Injectable({
@@ -9,51 +9,48 @@ import { User } from '../models/user.model';
 export class UsersService {
   private apiUrl = 'http://localhost:8080/api/users';
 
-  private users: User[] = [];
-
-  private usersSubject = new BehaviorSubject<User[]>(this.users);
-
   constructor(private http: HttpClient) {}
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.apiUrl).pipe(
+    return this.http.get<User[]>(`${this.apiUrl}/get-all`).pipe(
       map((users) =>
         users.map((user) => ({
           ...user,
           lastLogin: new Date(user.lastLogin).toISOString(),
         }))
-      )
+      ),
+      catchError(this.handleError)
     );
   }
 
-  getUserById(id: number): Observable<User | undefined> {
-    return this.getUsers().pipe(
-      map((users) => users.find((user) => user.id === id))
-    );
+  getUserById(id: number): Observable<User> {
+    return this.http
+      .get<User>(`${this.apiUrl}/get/${id}`)
+      .pipe(catchError(this.handleError));
   }
 
   addUser(user: Omit<User, 'id'>): Observable<User> {
-    return this.http.post<User>(this.apiUrl, user);
+    return this.http
+      .post<User>(`${this.apiUrl}/create`, user)
+      .pipe(catchError(this.handleError));
   }
 
   updateUser(id: number, user: Partial<User>): Observable<User> {
-    return this.http.put<User>(`${this.apiUrl}/${id}`, user);
+    return this.http
+      .put<User>(`${this.apiUrl}/update/${id}`, user)
+      .pipe(catchError(this.handleError));
   }
 
   deleteUser(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http
+      .delete<void>(`${this.apiUrl}/delete/${id}`)
+      .pipe(catchError(this.handleError));
   }
 
   deleteMultipleUsers(ids: number[]): Observable<void> {
-    return this.http.delete<void>(this.apiUrl, { body: ids }).pipe(
-      catchError(this.handleError)
-    );
-  }
-
-  getAllUsers(params?: any): Observable<any> {
-    return this.http.get<User[]>(this.apiUrl, { params }).pipe(
-      catchError(this.handleError)
-    );
+    return this.http
+      .delete<void>(`${this.apiUrl}/delete/batch`, { body: ids })
+      .pipe(catchError(this.handleError));
   }
 
   private handleError(error: HttpErrorResponse) {
